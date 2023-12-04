8c49a6061bd82b920d3df3264cb745cde69b7b9d
nosta <nostalgy6@gmail.com>
2023-12-04 17:11:43 +0900

    commit

---
+++ src/main/java/com/example/capdemo/CapdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.capdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/capdemo/CapdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
